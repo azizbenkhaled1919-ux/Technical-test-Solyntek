4cb15feffab60cf2511c6a3becd3bc0b428a7642
azizbenkhaled1919-ux <azizbenkhaled1919@gmail.com>
2026-06-05 16:59:15 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,9 @@
-output "cluster_name" {
-  value = aws_eks_cluster.solyntek.name
-}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.solyntek.endpoint
-}
-
 output "ecr_backend_url" {
-  value = aws_ecr_repository.backend.repository_url
+  description = "URL du repository ECR pour le backend"
+  value       = aws_ecr_repository.backend.repository_url
 }
 
 output "ecr_frontend_url" {
-  value = aws_ecr_repository.frontend.repository_url
+  description = "URL du repository ECR pour le frontend"
+  value       = aws_ecr_repository.frontend.repository_url
 }
